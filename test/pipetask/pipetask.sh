a8f1a5c85417c5d3825ee753752c03f3492d8916
T. Andrew Manning <manninga@illinois.edu>
2021-03-18 13:00:18 -0500

    Update pipetask.sh

--- test/pipetask/pipetask.sh
+++ test/pipetask/pipetask.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 # Load the software and environment configuration
 source "/opt/lsst/software/stack/loadLSST.bash"
 setup lsst_distrib
-export PYTHONPATH=$PYTHONPATH:/usr/lib64/python3.6/site-packages
+export PYTHONPATH=$PYTHONPATH:/usr/lib64/python3.8/site-packages
 
 export OUTPUT_COLLECTION="u/ocps/$JOB_ID"
 export PIPELINE_YAML="$PIPE_TASKS_DIR/pipelines/DRP.yaml#processCcd"
